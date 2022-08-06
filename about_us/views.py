9e6bbc98bd55c9143306fa85b2dc8a7b9d80a041
Vahid <kingvm8@gmail.com>
2022-08-06 22:26:33 +0430

    aboutus fullnames

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from accounts.models import User
+
 def about_us(request):
-    # View for about-us page
-    ...
+    query = User.objects.all()
+    return render(request, 'about_us.html', {'users': query})
