a40e66fee4b73d8d3d9d985e3b6748b354440f6a
Vahid <kingvm8@gmail.com>
2022-08-06 21:02:37 +0430

    account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'last_name', 'is_staff', 'is_active']
+    list_filter = ['gender', 'is_staff', 'is_superuser', 'is_active']
+    list_editable = ['is_staff', 'is_active']
+    list_display_links = ['username']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description')}),
+        ('Contact info', {'fields': ('phone', 'address')}), 
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
+        ('Important dates', {'fields': ('last_login', 'date_joined')}), 
+    )
